c3694e7cdfcaf5227f1762836a6fc1ff1d21051c
Cauã Souza <caua19922@gmail.com>
2025-04-01 18:22:35 -0300

    first commit

---
+++ money-converter/README.md
@@ -0,0 +1,6 @@
+# Money converter
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/joestarcsz/pen/RNwEopx](https://codepen.io/joestarcsz/pen/RNwEopx).
+
---
+++ money-converter/src/script.js
@@ -0,0 +1,44 @@
+/* Atribuições, valor em cada moeda */
+umD = 0.18
+umE = 0.15
+umI = 25
+umL = 0.13
+
+/* Conversor */
+document.getElementsByName("valorReal")[0].addEventListener("input", function() {
+  var valorReal = parseFloat (document.getElementsByName("valorReal")[0].value);
+
+if (!isNaN(valorReal)) {
+ 
+    var valorDolar = valorReal * umD;
+    var valorEuro = valorReal * umE;
+    var valorIene = valorReal * umI;
+    var valorLibra = valorReal * umL;
+  
+     valorDolar = valorDolar . toLocaleString ('pt-BR', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  });
+  valorEuro = valorEuro . toLocaleString ('pt-BR', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  });
+  valorIene = valorIene . toLocaleString ('pt-BR', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  });
+  valorLibra = valorLibra . toLocaleString ('pt-BR', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  });
+  
+      document.getElementsByName("valorDólar")[0].value = "USD " + valorDolar;
+    document.getElementsByName("valorEuro")[0].value = "€ " + valorEuro;
+    document.getElementsByName("valorIene")[0].value = "¥ " + valorIene;
+    document.getElementsByName("valorLibra")[0].value = "£ " + valorLibra; 
+    
+  } else {alert("Por Favor insira um número")}
+    
+});
+
+    
